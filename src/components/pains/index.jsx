844f97b32ba01d2fdde14c0c137902b7f46d942a
João Paulo <joaojpmoreira25@gmail.com>
2023-06-26 14:41:35 +0000

    feat: add pain page

--- src/components/pains/index.jsx
+++ src/components/pains/index.jsx
@@ -4,7 +4,8 @@ import './style.scss';
 
 import Section from '../shared/section'
 
-import { BsXCircleFill } from "react-icons/bs";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleXmark } from '@fortawesome/free-solid-svg-icons'
 
 const Pain = () => {
   return (
@@ -13,18 +14,21 @@ const Pain = () => {
 
           <div className="text">
             <strong>
-              Muitos profissionais sentem-se inseguros na hora de realizar uma Avaliação Física correta!
+              Foi entendendo e, mais do que isso, vivendo o dia a dia do consultório, que pude
+              perceber a real necessidade de mercado que muitos dos meus pacientes se
+              queixavam. Com isso, as reais lacunas que precisam ser preenchidas ficaram muito
+              claras para mim.
               <br />
-              Então se você:
+              Veja se você consegue se perceber nessas situações:
             </strong>
           </div>
 
           <div className="pains">
             <ul>
-              <li><BsXCircleFill size={24} color="red"/> Não tem acesso a um profissional de qualidade em sua cidade</li>
-              <li>Não tem tempo devido uma rotina muito corrida para passar por uma consulta presencial</li>
-              <li>Não se sente a vontade em passar em consulta presencial ou online</li>
-              <li>Não conseguiu resultado com nenhuma dieta milagrosa</li>
+              <li><FontAwesomeIcon icon={faCircleXmark} size="xl" style={{color: "#e0bb8d",}} /> Não tem acesso a um profissional de qualidade em sua cidade</li>
+              <li><FontAwesomeIcon icon={faCircleXmark} size="xl" style={{color: "#e0bb8d",}} /> Não tem tempo devido uma rotina muito corrida para passar por uma consulta presencial</li>
+              <li><FontAwesomeIcon icon={faCircleXmark} size="xl" style={{color: "#e0bb8d",}} /> Não se sente a vontade em passar em consulta presencial ou online</li>
+              <li><FontAwesomeIcon icon={faCircleXmark} size="xl" style={{color: "#e0bb8d",}} /> Não conseguiu resultado com nenhuma dieta milagrosa</li>
             </ul>
           </div>
         </div>
